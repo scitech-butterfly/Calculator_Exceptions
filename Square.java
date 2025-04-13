6e645ddd0f5b145c1eebefe66ca5326d5fe260c2
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 16:58:54 +0530

    Implemented Try-Catch for Square.java

--- Square.java
+++ Square.java
@@ -1,5 +1,16 @@
 // Square.java
+import java.util.*;
 
 public class Square{
-
+  public static void performSquare() {
+        try {
+            Scanner sc = new Scanner();
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+            double result = a * a;
+            System.out.println("Square = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in calculating square: " + e.getMessage());
+        }
+    }
 }
