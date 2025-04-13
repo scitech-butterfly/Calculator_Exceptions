1169ada54342aaa4c76fbb12693ec74207188979
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 17:02:20 +0530

    Implemented Try-Catch Block for Cube.java

--- Cube.java
+++ Cube.java
@@ -1,5 +1,17 @@
 // Cube.java
 
-public class Cube{
+import java.util.*;
 
+public class Cube{
+  public static void performCube() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+            double result = a * a * a;
+            System.out.println("Cube = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in calculating cube: " + e.getMessage());
+        }
+    }
 }
