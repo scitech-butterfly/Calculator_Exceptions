5c555c09573a23d1c568d173d1b7ae0d4a9685ea
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 16:52:59 +0530

    Added Try Block for Division.java

--- Division.java
+++ Division.java
@@ -1,5 +1,17 @@
 // Division.java
 
 public class Division{
+  try {
+            System.out.print("Enter numerator: ");
+            double a = sc.nextDouble();
+            System.out.print("Enter denominator: ");
+            double b = sc.nextDouble();
 
+            if (b == 0) {
+                throw new ArithmeticException("Cannot divide by zero.");
+            }
+
+            double result = a / b;
+            System.out.println("Result = " + result);
+  }
 }
