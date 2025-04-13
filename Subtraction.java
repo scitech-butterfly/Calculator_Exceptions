ca24c0ba3a8b128ff436c3daee994550b995ce83
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 17:45:30 +0530

    Added Imports in Subtraction.java

--- Subtraction.java
+++ Subtraction.java
@@ -1,8 +1,10 @@
 // Subtraction.java
+import java.util.*;
 
 public class Subtraction{
-  public static void performSubtraction(Scanner sc) {
+  public static void performSubtraction() {
         try {
+            Scanner sc = new Scanner(System.in);
             System.out.print("Enter first number: ");
             double a = sc.nextDouble();
             System.out.print("Enter second number: ");
