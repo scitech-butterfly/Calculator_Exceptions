b0d4ecd625e59ee095bdc4bdbe6c912e913e01cb
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 16:45:41 +0530

    Add Try-Catch for Addition.java

--- Addition.java
+++ Addition.java
@@ -1,5 +1,16 @@
 // Addition.java
 
 public class Addition{
-  
+  public static void performAddition(Scanner sc) {
+        try {
+            System.out.print("Enter first number: ");
+            double a = sc.nextDouble();
+            System.out.print("Enter second number: ");
+            double b = sc.nextDouble();
+            double result = a + b;
+            System.out.println("Result = " + result);
+        } catch (Exception e) {
+            System.out.println("Error in addition: " + e.getMessage());
+        }
+    }
 }
