ab422a4ba52b6268c99ddc1bf8454d99913788ec
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-04-13 17:42:47 +0530

    Implemented Try-Catch for SquareRoot.java

--- SquareRoot.java
+++ SquareRoot.java
@@ -1,5 +1,24 @@
 // SquareRoot.java
 
-public class SquareRoot{
+import java.util.*;
 
+public class SquareRoot{
+  public static void performSquareRoot() {
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.print("Enter a number: ");
+            double a = sc.nextDouble();
+
+            if (a < 0) {
+                throw new ArithmeticException("Cannot compute square root of a negative number.");
+            }
+
+            double result = Math.sqrt(a);
+            System.out.println("Square Root = " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("Math error: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("Error in calculating square root: " + e.getMessage());
+        }
+    }
 }
